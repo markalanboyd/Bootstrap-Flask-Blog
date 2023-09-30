0e0d0cee445fb8543178e23ab1a8846da09deee1
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-09-30 11:45:56 +0200

    Starting over to refactor
    I want to use a different structure to these files so I'm starting from scratch

--- app.py
+++ app.py
@@ -4,21 +4,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def get_all_posts():
     return render_template("index.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/post")
-def post():
 
 
 if __name__ == "__main__":
