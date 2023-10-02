ca62e7b40cfc35a850290b3496e587e564bebde5
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-10-02 19:51:22 +0200

    Integrating wtforms

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os
 import requests
-from flask import Flask, render_template
+import smtplib
+
+import dotenv
+from flask import Flask, render_template, request
 from datetime import datetime
+
+dotenv.load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PW = os.getenv("PW")
 
 app = Flask(__name__)
 
@@ -24,8 +33,37 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+def parse_message(request):
+    name = request.form["name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    message = request.form["message"]
+
+    email_content = f"""Contact submission form from {name}
+    From: {email}
+    Phone: {phone}
+    Message: {message}
+    """
+
+    return email_content
+
+
+def send_mail(from_email, from_pw, request):
+    email_content = parse_message(request)
+    smtp_server = "smtp.mail.yahoo.com"
+    smtp_port = 587
+    smtp_object = smtplib.SMTP(smtp_server, smtp_port)
+    smtp_object.starttls()
+    smtp_object.login(from_email, from_pw)
+    smtp_object.sendmail(from_email, from_email, email_content)
+    smtp_object.quit()
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        send_mail(EMAIL, PW, request)
+        return "Success!"
     return render_template("contact.html")
 
 
